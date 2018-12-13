6a17fe1e42e72e94c6a75f4522434266b7f7e6be
jcaracciolo <jcaracciolo@itba.edu.ar>
2018-12-13 17:46:02 -0300

    Added so you can't kill while dead

--- Assets/Scripts 1/Shoot.cs
+++ Assets/Scripts 1/Shoot.cs
@@ -6,12 +6,16 @@ public class Shoot : MonoBehaviour {
 	// Update is called once per frame
 	public float weaponRange = 50f;
 	private PlayerEventClient client;
+	private Health _health;
 
 	void Start() {
 		client = GameManager.Instance.eventClient;
+		_health = GetComponentInParent<Health>();
 	}
 	
 	void Update () {
+
+		if(_health.GetCurrentHealth() <=0 ) { return; }
 		if (Input.GetMouseButtonDown(0)) {
 			client.Shoot(weaponRange,transform);
 		}
